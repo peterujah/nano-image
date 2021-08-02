fda6957adaadcc5cbd5142b6593720d21d19085a
Chigozie peter <ujahchigozie@gmail.com>
2021-08-02 23:53:00 +0800

    Create upload.php

---
+++ test/upload.php
@@ -0,0 +1,29 @@
+```php
+<?php
+  if(isset($_FILES['image'])){
+    $ImageTemp = $_FILES['image']['tmp_name'];
+    $fileName = 'old-image.jpg'; 
+    $filePathAsset = __DIR__ . '/assets/img/nano/';
+    
+    if(!is_dir($filePathAsset)){
+      @mkdir($urlPathAsset, 0777, true);
+      @chmod($urlPathAsset, 0755);
+    }
+
+    if(file_exists($filePathAsset . $fileName)){
+      @unlink($filePathAsset . $fileName);
+    }
+
+    if (@move_uploaded_file($ImageTemp, $filePathAsset . $fileName)) {
+      $imagine = new  CustomGlobalClass\NanoImage();
+      $image = $imagine->open($logoPath . $fileName);
+
+      $image->resize(360, 200, false);
+      $image->save($filePathAsset . $imageName, false, 80, $imagine::JPEG);
+
+      $image->resize(116, 80, false);
+      $image->save($filePathAsset . $imageName, true, 100, $imagine::JPEG);
+
+      $image->free();
+    }
+  }
