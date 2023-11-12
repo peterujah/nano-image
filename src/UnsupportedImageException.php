c5f186f156d8dbb011da477c0f19c9795dc6f176
Chigozie Peter <ujahchigozie@gmail.com>
2023-11-13 03:39:16 +0800

    Create UnsupportedImageException.php

---
+++ src/UnsupportedImageException.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * @author      Peter Chigozie(NG) peterujah
+ * @copyright   Copyright (c), 2019 Peter(NG) peterujah
+ * @license     MIT public license
+ */
+namespace Peterujah\NanoBlock;
+use \Exception;
+class UnsupportedImageException extends Exception
+{
+    public function __construct($message = 'Unsupported image format', $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
