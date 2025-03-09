71045894b182a228de170d5b6d91e201480b39de
Chigozie Peter <ujahchigozie@gmail.com>
2025-03-09 21:00:04 +0800

    Update UnsupportedImageException.php

--- src/UnsupportedImageException.php
+++ src/UnsupportedImageException.php
@@ -8,12 +8,16 @@ namespace Peterujah\NanoBlock;
 use \Exception;
 class UnsupportedImageException extends Exception
 {
-    public function __construct(string $message = 'Unsupported image format', int $code = 0, Exception $previous = null)
+    public function __construct(
+        string $message = 'Unsupported image format', 
+        int $code = 0, 
+        ?Exception $previous = null
+    )
     {
         parent::__construct($message, $code, $previous);
     }
 
-    public function __toString()
+    public function __toString(): string
     {
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
